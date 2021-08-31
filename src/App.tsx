8c77e80690c387946ded9c2f3bf19ec59ca38544
chandrasekhar2039 <chandrasekhar.2039@gmail.com>
2021-08-31 18:20:15 +0530

    added lazy load(code splitting) with error boundaries, added Favicion and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import ApplyTheme from "./components/applyTheme";
+import React,{ Suspense } from "react";
 import './Styles/style.scss'
-import Main from "./components/main"
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import ErrorBoundary from "./ErrorBoundaries"
+
+const ApplyTheme = React.lazy(() => import("./components/applyTheme"));
+const Main = React.lazy(() => import("./components/main"));
+
+
+
+
 function App() {
   return (
-    <ApplyTheme>
-      <ToastContainer toastClassName="toast" />
-      <Main/>
-    </ApplyTheme>
+    <Suspense fallback={<div className="loader">Loading...</div>}>
+      <ErrorBoundary>
+        <ApplyTheme>
+          <ToastContainer toastClassName="toast" />
+          <Main/>
+        </ApplyTheme>
+      </ErrorBoundary>
+      </Suspense>
   );
 }
 
---
+++ src/ErrorBoundaries.tsx
@@ -0,0 +1,37 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <div className="error">
+                <h1>Something went wrong ! 🙁</h1>
+                <p>Make sure you have a stable internet connection then <strong>Try reloading the Page .</strong><br/> If the problem still persists <i><a href="mailto:chandrasekhar.2039@gmail.com">Conatct here.</a></i></p>
+            </div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
